bb669c7bcaa26735a1d98798f6232d670d65e86b
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-12-27 23:57:50 +0100

    Avoid big closures, use @bind to bind context

--- src/rules/no-unused-label.ts
+++ src/rules/no-unused-label.ts
@@ -1,6 +1,7 @@
 import * as ts from 'typescript';
 import { isLabeledStatement, isIterationStatement, isBreakOrContinueStatement, isFunctionScopeBoundary } from 'tsutils';
 import { AbstractRule, Replacement } from '../types';
+import bind from 'bind-decorator';
 
 interface Label {
     node: ts.LabeledStatement;
@@ -9,44 +10,47 @@ interface Label {
 }
 
 export class Rule extends AbstractRule {
+    private labels: Label[] = [];
+
     public static supports(sourceFile: ts.SourceFile) {
         return !sourceFile.isDeclarationFile;
     }
-    public apply() {
-        let labels: Label[] = [];
-        const cb = (node: ts.Node): void => {
-            if (isLabeledStatement(node)) {
-                if (!isIterationStatement(node.statement) && node.statement.kind !== ts.SyntaxKind.SwitchStatement) {
-                    this.fail(node);
-                    return cb(node.statement);
-                }
-                labels.unshift({node, name: node.label.text, used: false});
-                cb(node.statement);
-                const label = labels.shift()!;
-                if (!label.used)
-                    this.fail(label.node);
-                return;
-            }
-            if (isBreakOrContinueStatement(node)) {
-                if (node.label !== undefined) {
-                    const name = node.label.text;
-                    const label = labels.find((l) => l.name === name);
-                    if (label !== undefined)
-                        label.used = true;
-                }
-                return;
-            }
-            if (labels.length !== 0 && isFunctionScopeBoundary(node)) {
-                const saved = labels;
-                labels = [];
-                ts.forEachChild(node, cb);
-                labels = saved;
-                return;
-            }
-            return ts.forEachChild(node, cb);
-        };
 
-        return this.sourceFile.statements.forEach(cb);
+    public apply() {
+        return this.sourceFile.statements.forEach(this.visitNode);
+    }
+
+    @bind
+    private visitNode(node: ts.Node): void {
+        if (isLabeledStatement(node)) {
+            if (!isIterationStatement(node.statement) && node.statement.kind !== ts.SyntaxKind.SwitchStatement) {
+                this.fail(node);
+                return this.visitNode(node.statement);
+            }
+            this.labels.unshift({node, name: node.label.text, used: false});
+            this.visitNode(node.statement);
+            const label = this.labels.shift()!;
+            if (!label.used)
+                this.fail(label.node);
+            return;
+        }
+        if (isBreakOrContinueStatement(node)) {
+            if (node.label !== undefined) {
+                const name = node.label.text;
+                const label = this.labels.find((l) => l.name === name);
+                if (label !== undefined)
+                    label.used = true;
+            }
+            return;
+        }
+        if (this.labels.length !== 0 && isFunctionScopeBoundary(node)) {
+            const saved = this.labels;
+            this.labels = [];
+            ts.forEachChild(node, this.visitNode);
+            this.labels = saved;
+            return;
+        }
+        return ts.forEachChild(node, this.visitNode);
     }
 
     private fail(statement: ts.LabeledStatement) {
