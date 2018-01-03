2af95b7e986394e9c0435eea75fccd0787a09997
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-01-03 16:03:13 +0100

    await-promise: check for-await with non-AsyncIterable value

--- src/rules/await-promise.ts
+++ src/rules/await-promise.ts
@@ -1,7 +1,7 @@
 import { injectable } from 'inversify';
 import { TypedRule, FlattenedAst, TypedRuleContext, Replacement } from '../types';
 import * as ts from 'typescript';
-import { isAwaitExpression, isUnionType } from 'tsutils';
+import { isAwaitExpression, isUnionType, isForOfStatement } from 'tsutils';
 
 @injectable()
 export class Rule extends TypedRule {
@@ -14,14 +14,24 @@ export class Rule extends TypedRule {
     }
 
     public apply() {
-        for (const node of this.flatAst)
-            if (isAwaitExpression(node) && !this.isPromiseLike(node.expression))
+        for (const node of this.flatAst) {
+            if (isAwaitExpression(node)) {
+                if (!this.isPromiseLike(node.expression))
+                    this.addFailure(
+                        node.expression.pos - 'await'.length,
+                        node.end,
+                        "Unnecessary 'await' of a non-Promise value.",
+                        Replacement.delete(node.expression.pos - 'await'.length, node.expression.getStart(this.sourceFile)),
+                    );
+            } else if (isForOfStatement(node) && node.awaitModifier !== undefined && !this.isAsyncIterable(node.expression)) {
                 this.addFailure(
-                    node.expression.pos - 'await'.length,
-                    node.end,
-                    "Unnecessary 'await' of a non-Promise value.",
-                    Replacement.delete(node.expression.pos - 'await'.length, node.expression.getStart(this.sourceFile)),
+                    node.getStart(this.sourceFile),
+                    node.statement.pos,
+                    "Unnecessary 'for await' of a non-AsyncIterable value.",
+                    Replacement.delete(node.awaitModifier.pos, node.awaitModifier.end),
                 );
+            }
+        }
     }
 
     private isPromiseLike(node: ts.Expression): boolean {
@@ -43,5 +53,26 @@ export class Rule extends TypedRule {
     private isThenable(type: ts.Type, node: ts.Node): boolean {
         const then = type.getProperty('then');
         return then !== undefined && this.checker.getTypeOfSymbolAtLocation(then, node).getCallSignatures().length !== 0;
+    }
+
+    private isAsyncIterable(node: ts.Expression): boolean {
+        const type = this.checker.getApparentType(this.checker.getTypeAtLocation(node));
+        if (type.flags & ts.TypeFlags.Any)
+            return true;
+        if (!isUnionType(type))
+            return this.hasSymbolAsyncIterator(type);
+        for (const t of type.types)
+            if (this.hasSymbolAsyncIterator(t))
+                return true;
+        return false;
+    }
+
+    /**
+     * We consider a type as AsyncIterable when it has a property key [Symbol.asyncIterator].
+     * The spec requires this property to be a function returning an object with a `next` method which returns a Promise of IteratorResult.
+     * But that's none of our business as the compiler already does the heavy lifting.
+     */
+    private hasSymbolAsyncIterator(type: ts.Type): boolean {
+        return type.getProperties().some((prop) => prop.name === '__@asyncIterator'); // TODO this may break in future typescript releases
     }
 }
