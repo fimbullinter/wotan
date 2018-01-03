8ba97896520d1a6e986eaf1d69e0d82ede369e6b
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-01-03 22:14:42 +0100

    More documentation

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,6 +1,8 @@
 export = {
     rules: {
+        'await-promise': 'error',
         'no-debugger': 'error',
+        'no-return-await': 'error',
         'no-unused-label': 'error',
         'no-useless-assertion': 'error',
         'trailing-newline': 'error',
