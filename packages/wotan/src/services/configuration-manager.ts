085f75fc923928f141d7c46bebe8dfbf8510703e
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-06 22:29:46 +0100

    move functions to reduce config to ConfigurationManager

--- packages/wotan/src/services/configuration-manager.ts
+++ packages/wotan/src/services/configuration-manager.ts
@@ -8,11 +8,13 @@ import {
     GlobalSettings,
     DirectoryService,
     ConfigurationProvider,
+    EffectiveConfiguration,
 } from '../types';
 import * as path from 'path';
 import { ConfigurationError } from '../error';
 import { resolveCachedResult } from '../utils';
-import { reduceConfigurationForFile, getProcessorForFile, getSettingsForFile } from '../configuration';
+import { Minimatch } from 'minimatch';
+import * as isNegated from 'is-negated-glob';
 
 const configCache = new CacheIdentifier<string, Configuration>('configuration');
 
@@ -50,15 +52,19 @@ export class ConfigurationManager {
     }
 
     public reduce(config: Configuration, file: string): ReducedConfiguration | undefined {
-        return reduceConfigurationForFile(config, file, this.directories.getCurrentDirectory());
+        return reduceConfig(
+            config,
+            path.resolve(this.directories.getCurrentDirectory(), file),
+            {rules: new Map(), settings: new Map(), processor: undefined},
+        );
     }
 
     public getProcessor(config: Configuration, fileName: string): string | undefined {
-        return getProcessorForFile(config, fileName, this.directories.getCurrentDirectory());
+        return findProcessorInConfig(config, path.resolve(this.directories.getCurrentDirectory(), fileName)) || undefined;
     }
 
     public getSettings(config: Configuration, fileName: string): GlobalSettings {
-        return getSettingsForFile(config, fileName, this.directories.getCurrentDirectory());
+        return reduceSettings(config, path.resolve(this.directories.getCurrentDirectory(), fileName), new Map());
     }
 
     public load(fileName: string): Configuration {
@@ -81,4 +87,96 @@ export class ConfigurationManager {
             throw new ConfigurationError(`Error loading ${stack.join(' => ')}: ${e && e.message}`);
         }
     }
+}
+
+function reduceConfig(config: Configuration, filename: string, receiver: ReducedConfiguration): ReducedConfiguration | undefined {
+    const relativeFilename = path.relative(path.dirname(config.filename), filename);
+    if (config.exclude !== undefined && matchesGlobs(relativeFilename, config.exclude))
+        return;
+    for (const base of config.extends)
+        if (reduceConfig(base, filename, receiver) === undefined)
+            return;
+
+    extendConfig(receiver, config);
+
+    if (config.overrides !== undefined)
+        for (const override of config.overrides)
+            if (matchesGlobs(relativeFilename, override.files))
+                extendConfig(receiver, override);
+
+    return receiver;
+}
+
+function extendConfig(receiver: ReducedConfiguration, config: Configuration | Configuration.Override) {
+    if (config.processor !== undefined)
+        receiver.processor = config.processor || undefined;
+    if (config.settings !== undefined)
+        extendSettings(receiver.settings, config.settings);
+    if (config.rules !== undefined)
+        extendRules(receiver.rules, config.rules);
+}
+
+function matchesGlobs(file: string, patterns: ReadonlyArray<string>): boolean {
+    for (let i = patterns.length - 1; i >= 0; --i) {
+        const glob = isNegated(patterns[i]);
+        const local = glob.pattern.startsWith('./');
+        if (local)
+            glob.pattern = glob.pattern.substr(2);
+        if (new Minimatch(glob.pattern, {matchBase: !local}).match(file))
+            return !glob.negated;
+    }
+    return false;
+}
+
+function extendRules(
+    receiver: EffectiveConfiguration['rules'],
+    rules: ReadonlyMap<string, Configuration.RuleConfig>,
+) {
+    for (const [ruleName, config] of rules) {
+        const prev = receiver.get(ruleName);
+        receiver.set(ruleName, {
+            severity: 'error',
+            options: undefined,
+            ...prev,
+            ...config,
+        });
+    }
+}
+
+function extendSettings(receiver: EffectiveConfiguration['settings'], settings: ReadonlyMap<string, any>) {
+    for (const [key, value] of settings)
+        receiver.set(key, value);
+}
+
+function findProcessorInConfig(config: Configuration, fileName: string): string | undefined | null | false {
+    if (config.overrides !== undefined) {
+        const relative = path.relative(path.dirname(config.filename), fileName);
+        for (let i = config.overrides.length - 1; i >= 0; --i) {
+            const override = config.overrides[i];
+            if (override.processor !== undefined && matchesGlobs(relative, override.files))
+                return override.processor;
+        }
+    }
+    if (config.processor !== undefined)
+        return config.processor;
+    for (let i = config.extends.length - 1; i >= 0; --i) {
+        const processor = findProcessorInConfig(config.extends[i], fileName);
+        if (processor !== undefined)
+            return processor;
+    }
+    return;
+}
+
+function reduceSettings(config: Configuration, fileName: string, receiver: Map<string, any>) {
+    for (const base of config.extends)
+        reduceSettings(base, fileName, receiver);
+    if (config.settings !== undefined)
+        extendSettings(receiver, config.settings);
+    if (config.overrides !== undefined) {
+        const relative = path.relative(path.dirname(config.filename), fileName);
+        for (const override of config.overrides)
+            if (override.settings && matchesGlobs(relative, override.files))
+                extendSettings(receiver, override.settings);
+    }
+    return receiver;
 }
