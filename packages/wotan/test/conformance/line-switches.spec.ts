5dc2eddb64aa637fba1b7140526cbb2d0651a506
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-08 10:08:17 +0100

    completely test LineSwitchService

--- packages/wotan/test/conformance/line-switches.spec.ts
+++ packages/wotan/test/conformance/line-switches.spec.ts
@@ -5,14 +5,17 @@ import { LineSwitchService, DisableMap } from '../../src/services/line-switches'
 import { DefaultLineSwitchParser } from '../../src/services/default/line-switch-parser';
 import { convertAst } from 'tsutils';
 
+
 test('getDisabledRanges', (t) => {
-    const lineSwitchService = new LineSwitchService(new DefaultLineSwitchParser());
     const source = `#! shebang
 // wotan-disable
 "/* wotan-enable */" /*wotan-enable*/;
 let foo /* wotan-disable-line */ = true;
 //wotan-disable-line foobaz`;
     const sourceFile = ts.createSourceFile('/foo.ts', source, ts.ScriptTarget.ESNext);
+    const {wrapped} = convertAst(sourceFile);
+
+    const lineSwitchService = new LineSwitchService(new DefaultLineSwitchParser());
     const expected: DisableMap = new Map([['foobar', [
         {
             pos: 11,
@@ -30,8 +33,18 @@ let foo /* wotan-disable-line */ = true;
     );
 
     t.deepEqual(
-        lineSwitchService.getDisabledRanges(sourceFile, ['foobar'], () => convertAst(sourceFile).wrapped),
+        lineSwitchService.getDisabledRanges(sourceFile, ['foobar'], () => wrapped),
         expected,
         'with WrappedAst',
+    );
+
+    t.deepEqual(
+        new LineSwitchService({
+            parse(_source, _rules, context) {
+                t.is(context.getCommentAtPosition(-1), undefined); // should not throw here
+                return new Map([['foo', [{enable: true, position: 0}]], ['bar', []]]);
+            },
+        }).getDisabledRanges(sourceFile, ['foo'], () => wrapped),
+        new Map(),
     );
 });
