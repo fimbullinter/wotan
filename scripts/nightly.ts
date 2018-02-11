db0f04fad8becf7ca56810a096c92c98fb9cc93a
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-11 22:22:49 +0100

    put together nightly release script

---
+++ scripts/nightly.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs';
+import * as cp from 'child_process';
+
+interface Dependencies {
+    [name: string]: string;
+}
+
+interface PackageData {
+    name: string;
+    version: string;
+    private?: boolean;
+    dependencies?: Dependencies;
+    devDependencies?: Dependencies;
+    peerDependencies?: Dependencies;
+}
+
+const currentDate = new Date();
+const version = // tslint:disable-next-line
+    `${require('../package.json').version}-dev.${currentDate.getFullYear() * 10000 + (currentDate.getMonth() + 1) * 100 + currentDate.getDate()}`;
+
+const packages = new Map(
+    fs.readdirSync('packages').map((name): [string, PackageData] => {
+        return [name, require(`../packages/${name}/package.json`)];
+    }),
+);
+const publicPackages = new Map(Array.from(packages).filter((v) => !v[1].private));
+
+const changed = new Set<string>();
+function markChanged(name: string) {
+    if (changed.has(name))
+        return;
+    changed.add(name);
+    const manifest = packages.get(name)!;
+    manifest.version = version;
+    for (const [k, v] of publicPackages) {
+        if (v.dependencies && v.dependencies[manifest.name]) {
+            v.dependencies[manifest.name] = version;
+            markChanged(k);
+        }
+        if (v.peerDependencies && v.peerDependencies[manifest.name]) {
+            v.peerDependencies[manifest.name] = version;
+            markChanged(k);
+        }
+    }
+}
+
+console.log(process.env.TRAVIS_COMMIT_RANGE);
+const diff = cp.execSync(
+    `git diff --name-only -- packages/${Array.from(publicPackages.keys()).join(' packages/')}`,
+    {encoding: 'utf8'},
+).trim();
+if (diff !== '')
+    for (const file of diff.split(/\r?\n/))
+        markChanged(file.split(/[/\\]/)[1]);
+
+for (const name of changed) {
+    fs.writeFileSync(`packages/${name}/package.json`, JSON.stringify(publicPackages.get(name), undefined, 2) + '\n');
+    console.log(`npm publish packages/${name} --tag next`);
+}
