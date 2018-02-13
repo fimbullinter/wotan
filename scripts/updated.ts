7421bbad046564bb1e76625c1918f68845731c4d
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-13 15:34:58 +0100

    Add script to show the changed public packages since the last release

---
+++ scripts/updated.ts
@@ -0,0 +1,9 @@
+import { getChangedPackageNames, getLastRelaseTag, getPackages } from './util';
+
+// Usage: node scripts/updated [rev]
+// rev can be any valid git revision
+// if omitted, it defaults to the last release tag
+
+const since = process.argv[2] || getLastRelaseTag();
+
+console.log(Array.from(getChangedPackageNames(since, getPackages().publicPackages.keys())).join(' '));
