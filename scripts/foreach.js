0f906c1b1a9e52254a74a69ec72c745473ad38e9
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-14 20:47:00 +0100

    maintain scripts/foreach only as js file

--- scripts/foreach.js
+++ scripts/foreach.js
@@ -1,9 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const glob = require("glob");
-const spawn = require("cross-spawn");
-const chalk_1 = require("chalk");
-const os = require("os");
+const glob =require('glob');
+const spawn = require('cross-spawn');
+const chalk = require('chalk').default;
+const os = require('os');
+
 let bail = false;
 let i = 2;
 outer: for (; i < process.argv.length; ++i) {
@@ -15,11 +14,15 @@ outer: for (; i < process.argv.length; ++i) {
             bail = true;
     }
 }
+
 const [pattern, ...cmds] = process.argv.slice(i);
+
 glob(trimSingleQuotes(pattern), (err, matches) => {
     if (err)
         throw err;
+    /** @type {Array<string>} */
     let queue = [];
+    /** @type {Array<{cmd: string, code: number}>} */
     let failed = [];
     for (const match of matches)
         for (const cmd of cmds)
@@ -32,35 +35,43 @@ glob(trimSingleQuotes(pattern), (err, matches) => {
         const cmd = queue.shift();
         if (cmd === undefined)
             return;
-        console.log(chalk_1.default.grey('$ ' + cmd));
-        const p = spawn(cmd, undefined, { shell: true, stdio: 'inherit' })
+        console.log(chalk.grey('$ ' + cmd));
+        const p = spawn(cmd, undefined, {shell: true, stdio: 'inherit'})
             .on('exit', (code) => {
-            running.delete(p);
-            if (code) {
-                process.exitCode = code;
-                failed.push({ cmd, code });
-                if (bail) {
-                    for (const active of running)
-                        active.kill();
-                    running.clear();
-                    queue = [];
+                running.delete(p);
+                if (code) {
+                    process.exitCode = code;
+                    failed.push({cmd, code});
+                    if (bail) {
+                        for (const active of running)
+                            active.kill();
+                        running.clear();
+                        queue = [];
+                    }
                 }
-            }
-            next();
-            if (running.size === 0 && failed.length !== 0) {
-                console.log(chalk_1.default.red('FAILED'));
-                for (const fail of failed)
-                    console.log(fail.code, fail.cmd);
-                failed = [];
-            }
-        });
+                next();
+                if (running.size === 0 && failed.length !== 0) {
+                    console.log(chalk.red('FAILED'));
+                    for (const fail of failed)
+                        console.log(fail.code, fail.cmd);
+                    failed = [];
+                }
+            });
         running.add(p);
     }
 });
+
+/**
+ * @param {string} str
+ */
 function trimSingleQuotes(str) {
     return str.startsWith("'") && str.endsWith("'") ? str.slice(1, -1) : str;
 }
+
+/**
+ * @param {string} cmd
+ * @param {string} file
+ */
 function formatCommand(cmd, file) {
     return cmd.replace('{{file}}', file);
 }
-//# sourceMappingURL=foreach.js.map
