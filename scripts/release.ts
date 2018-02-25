c27592d8384a8be7425125ad1261dc1e2759d14c
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-25 21:32:00 +0100

    v0.3.0

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,15 +4,19 @@ import {
     execAndLog,
     ensureCleanTree,
 } from './util';
+import { SemVer } from 'semver';
 
 ensureCleanTree();
+
+const semver = new SemVer(require('../package.json').version);
+const currentVersion = `${semver.major}.${semver.minor - 1}.0`;
 
 const {publicPackages} = getPackages();
 
 const needsRelease = getChangedPackageNames('HEAD^', publicPackages.keys());
 
 for (const [name, manifest] of publicPackages) {
-    if (needsRelease.has(name)) {
+    if (manifest.version === currentVersion && needsRelease.has(name)) {
         execAndLog(`npm publish packages/${name} --tag latest ${process.argv.slice(2).join(' ')}`);
         execAndLog(`npm dist-tag add ${manifest.name}@${manifest.version} next`);
     }
