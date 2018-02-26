6006576bc2af7799db32f7deb906691eff7f5100
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-02-26 13:36:45 +0100

    no-useless-initializer: do not apply in declaration files

--- packages/wotan/src/rules/no-useless-initializer.ts
+++ packages/wotan/src/rules/no-useless-initializer.ts
@@ -3,6 +3,10 @@ import * as ts from 'typescript';
 import { isIdentifier, getChildOfKind, isFunctionWithBody, isUnionTypeNode, getPreviousToken, getNextToken } from 'tsutils';
 
 export class Rule extends AbstractRule {
+    public static supports(sourceFile: ts.SourceFile) {
+        return !sourceFile.isDeclarationFile;
+    }
+
     public apply() {
         const isJs = this.sourceFile.flags & ts.NodeFlags.JavaScriptFile;
         for (const node of this.context.getFlatAst()) {
