3bb8aff527be69e66a15b7506ce6e66ca6102ffb
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-03-05 13:16:34 +0100

    check loops and if statements with constant condition

--- packages/wotan/src/rules/no-unreachable-code.ts
+++ packages/wotan/src/rules/no-unreachable-code.ts
@@ -1,6 +1,15 @@
 import { AbstractRule } from '../types';
 import * as ts from 'typescript';
-import { isBlockScopedVariableDeclarationList, endsControlFlow, isBlock, isCaseOrDefaultClause, hasModifier } from 'tsutils';
+import {
+    isBlockScopedVariableDeclarationList,
+    hasModifier,
+    isBlock,
+    getControlFlowEnd,
+    endsControlFlow,
+    isLabeledStatement,
+} from 'tsutils';
+
+type ForDoWhileStatement = ts.ForStatement | ts.WhileStatement | ts.DoStatement;
 
 export class Rule extends AbstractRule {
     public static supports(sourceFile: ts.SourceFile) {
@@ -9,33 +18,109 @@ export class Rule extends AbstractRule {
 
     public apply() {
         for (const node of this.context.getFlatAst()) {
-            if (!isBlock(node) && !isCaseOrDefaultClause(node))
-                continue;
-            let i = node.statements.findIndex(endsControlFlow);
-            if (i === -1)
-                continue;
-            for (i += 1; i < node.statements.length; ++i) {
-                if (!isHoistedDeclaration(node.statements[i])) {
-                    this.addFailureAtNode(node.statements[i].getFirstToken(), 'Unreachable code detected.');
+            switch (node.kind) {
+                case ts.SyntaxKind.Block:
+                case ts.SyntaxKind.CaseClause:
+                case ts.SyntaxKind.DefaultClause:
+                    this.checkBlock(<ts.BlockLike>node);
                     break;
-                }
+                case ts.SyntaxKind.IfStatement:
+                    this.checkIfStatement(<ts.IfStatement>node);
+                    break;
+                case ts.SyntaxKind.ForStatement:
+                case ts.SyntaxKind.WhileStatement:
+                    this.checkConstantIterationCondition(<ForDoWhileStatement>node);
             }
         }
     }
+
+    private checkBlock(node: ts.BlockLike) {
+        let i = node.statements.findIndex(nextStatementIsUnreachable);
+        if (i === -1 || i === node.statements.length - 1)
+            return;
+        for (i += 1; i < node.statements.length; ++i)
+            if (isExecutableStatement(node.statements[i]))
+                return this.report(node.statements[i]);
+    }
+
+    private checkIfStatement(node: ts.IfStatement) {
+        switch (getConstantCondition(node.expression)) {
+            case false:
+                return this.report(node.thenStatement);
+            case true:
+                if (node.elseStatement !== undefined)
+                    this.report(node.elseStatement);
+        }
+    }
+
+    private checkConstantIterationCondition(node: ForDoWhileStatement) {
+        if (getConstantIterationCondition(node) === false)
+            this.report(node.statement);
+    }
+
+    private report(node: ts.Statement) {
+        while (isBlock(node)) {
+            const next = node.statements.find(isExecutableStatement);
+            if (next === undefined)
+                return;
+            node = next;
+        }
+        this.addFailureAtNode(node.getFirstToken(this.sourceFile), 'Unreachable code detected.');
+    }
 }
 
-function isHoistedDeclaration(node: ts.Statement) {
+function nextStatementIsUnreachable(statement: ts.Statement): boolean {
+    if (endsControlFlow(statement))
+        return true;
+    const labels: string[] = [];
+    while (isLabeledStatement(statement)) {
+        labels.push(statement.label.text);
+        statement = statement.statement;
+    }
+    switch (statement.kind) {
+        case ts.SyntaxKind.ForStatement:
+        case ts.SyntaxKind.WhileStatement:
+        case ts.SyntaxKind.DoStatement:
+            // statements after loops, that go on forever without breaking, are never executed
+            return getConstantIterationCondition(<ForDoWhileStatement>statement) === true &&
+                !getControlFlowEnd((<ts.IterationStatement>statement).statement).statements.some(
+                    (jump) => jump.kind === ts.SyntaxKind.BreakStatement && (jump.label === undefined || labels.includes(jump.label.text)),
+                );
+    }
+    return false;
+}
+
+function isExecutableStatement(node: ts.Statement): boolean {
     switch (node.kind) {
         case ts.SyntaxKind.FunctionDeclaration:
         case ts.SyntaxKind.InterfaceDeclaration:
         case ts.SyntaxKind.TypeAliasDeclaration:
-            return true;
-        case ts.SyntaxKind.EnumDeclaration:
-            return hasModifier(node.modifiers, ts.SyntaxKind.ConstKeyword);
-        case ts.SyntaxKind.VariableStatement:
-            return isBlockScopedVariableDeclarationList((<ts.VariableStatement>node).declarationList) &&
-            (<ts.VariableStatement>node).declarationList.declarations.every((d) => d.initializer === undefined);
-        default:
             return false;
+        case ts.SyntaxKind.EnumDeclaration:
+            return !hasModifier(node.modifiers, ts.SyntaxKind.ConstKeyword);
+        case ts.SyntaxKind.VariableStatement:
+            return isBlockScopedVariableDeclarationList((<ts.VariableStatement>node).declarationList) ||
+                (<ts.VariableStatement>node).declarationList.declarations.some((d) => d.initializer !== undefined);
+        case ts.SyntaxKind.Block:
+            return (<ts.Block>node).statements.some(isExecutableStatement);
+        default:
+            return true;
+    }
+}
+
+function getConstantIterationCondition(statement: ForDoWhileStatement) {
+    return statement.kind === ts.SyntaxKind.ForStatement
+        ? statement.condition === undefined || getConstantCondition(statement.condition)
+        : getConstantCondition(statement.expression);
+}
+
+function getConstantCondition(node: ts.Expression) {
+    switch (node.kind) {
+        case ts.SyntaxKind.TrueKeyword:
+            return true;
+        case ts.SyntaxKind.FalseKeyword:
+            return false;
+        default:
+            return;
     }
 }
