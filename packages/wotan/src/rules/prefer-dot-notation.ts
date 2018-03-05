bf1e746549e35377ade16be18ba6ba38b45c35d8
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-03-05 17:52:05 +0100

    new rule: prefer-dot-notation (#111)

---
+++ packages/wotan/src/rules/prefer-dot-notation.ts
@@ -0,0 +1,24 @@
+import { AbstractRule, Replacement } from '../types';
+import * as ts from 'typescript';
+import { isElementAccessExpression, isTextualLiteral, isValidPropertyAccess } from 'tsutils';
+
+export class Rule extends AbstractRule {
+    public static supports(sourceFile: ts.SourceFile) {
+        return !sourceFile.isDeclarationFile;
+    }
+
+    public apply() {
+        for (const node of this.context.getFlatAst()) {
+            if (isElementAccessExpression(node) && node.argumentExpression !== undefined &&
+                isTextualLiteral(node.argumentExpression) && isValidPropertyAccess(node.argumentExpression.text)) {
+                // for compatibility with typescript@2.4
+                const property = ts.unescapeIdentifier(node.argumentExpression.text); // wotan-disable-line no-unstable-api-use
+                this.addFailureAtNode(
+                    node.argumentExpression,
+                    `Prefer 'obj.${property}' over 'obj[${node.argumentExpression.getText(this.sourceFile)}]'.`,
+                    Replacement.replace(node.expression.end, node.end, '.' + property),
+                );
+            }
+        }
+    }
+}
