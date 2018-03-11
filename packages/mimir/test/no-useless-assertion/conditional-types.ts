9f4bcd13f39699b88900334236ef979abfc33238
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-03-11 17:02:20 +0100

    no-useless-assertion: add test with conditional types
    Fixes: #116

---
+++ packages/mimir/test/no-useless-assertion/conditional-types.ts
@@ -0,0 +1,34 @@
+export {};
+
+type Nullable<T> = T extends null | undefined ? T : T | null | undefined;
+type NonNullable<T> = T extends null | undefined ? never : T;
+
+declare function nullable<T>(param: T): Nullable<T>;
+declare function nonNullable<T>(param: T): NonNullable<T>;
+
+declare function takeString(param: string): void;
+declare function takeNullable(param: Nullable<string>): void;
+
+declare let v: Nullable<string>;
+declare let s: NonNullable<typeof v>;
+
+takeString(v!);
+takeNullable(v!);
+takeString(s!);
+takeNullable(s!);
+takeNullable(nullable(s)!);
+takeString(nonNullable(v)!);
+
+nullable(s)!;
+nonNullable(s)!;
+nullable(null)!;
+nonNullable(null)!;
+
+nullable(s) as string;
+nullable(s) as string | null | undefined;
+nonNullable(s) as string;
+
+v as NonNullable<typeof s>;
+s as Nullable<string>;
+s as NonNullable<string>;
+(Boolean() ? s : null) as NonNullable<string>;
