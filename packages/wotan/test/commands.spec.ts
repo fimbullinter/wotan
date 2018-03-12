542a4e5424a1a034e080b3f2423edfe087c0957b
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-03-12 06:17:27 -0700

    Fix test, update baseline

--- packages/wotan/test/commands.spec.ts
+++ packages/wotan/test/commands.spec.ts
@@ -144,8 +144,8 @@ test('ShowCommand', async (t) => {
     }
     function normalizePaths(str: string): string {
         // replace `cwd` with / and all backslashes with forward slash
-        const re = new RegExp(`'?${escapeRegex(cwd)}(.*?)'?$`, 'gm');
-        return str.replace(/\\\\/g, '\\').replace(re, (_, p) => unixifyPath(p));
+        const re = new RegExp(`(['"]?)${escapeRegex(cwd)}(.*?)\\1(,?)$`, 'gm');
+        return str.replace(/\\\\/g, '\\').replace(re, (_, q, p, c) => q + unixifyPath(p) + q + c);
     }
 });
 
