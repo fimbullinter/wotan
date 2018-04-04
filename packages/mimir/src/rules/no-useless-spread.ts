9655c274289e1d6648b4cddb3448324bee3d8c26
Aaron Ervin <aervin@uste3.com>
2018-04-04 11:01:06 -0400

    further optimized using regex to find all spread operators

--- packages/mimir/src/rules/no-useless-spread.ts
+++ packages/mimir/src/rules/no-useless-spread.ts
@@ -1,11 +1,19 @@
 import * as ts from 'typescript';
-import { AbstractRule, Replacement } from '@fimbul/ymir';
+import { AbstractRule, Replacement, excludeDeclarationFiles } from '@fimbul/ymir';
+import { WrappedAst, getWrappedNodeAtPosition } from 'tsutils';
 
 const FAILURE_STRING = 'Using the spread operator here is not necessary.';
 
+@excludeDeclarationFiles
 export class Rule extends AbstractRule {
     public apply() {
-        for (const node of this.context.getFlatAst())
+        const re = /\.{3}\s*[/[{]/g;
+        let wrappedAst: WrappedAst | undefined;
+        for (let match = re.exec(this.sourceFile.text); match !== null; match = re.exec(this.sourceFile.text)) {
+            const { node } = getWrappedNodeAtPosition(
+                wrappedAst || (wrappedAst = this.context.getWrappedAst()),
+                match.index,
+            )!;
             if (
                 (node.kind === ts.SyntaxKind.SpreadElement &&
                     (<ts.SpreadElement>node).expression.kind === ts.SyntaxKind.ArrayLiteralExpression) ||
@@ -13,10 +21,11 @@ export class Rule extends AbstractRule {
                     (<ts.SpreadAssignment>node).expression.kind === ts.SyntaxKind.ObjectLiteralExpression)
             )
                 this.addFailureAtNode(node, FAILURE_STRING, removeUselessSpread(<ts.SpreadElement>node));
+        }
     }
 }
 
-function removeUselessSpread(node: ts.SpreadElement): Replacement[] {
+function removeUselessSpread(node: ts.SpreadElement | ts.SpreadAssignment): Replacement[] {
     return [
         Replacement.delete(node.getStart(), node.expression.getStart() + 1),
         Replacement.delete(node.expression.end - 1, node.expression.end),
