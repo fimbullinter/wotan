19edb31f466afea95f2fb564c9299cde03fb9609
Aaron Ervin <aervin@uste3.com>
2018-04-03 20:57:06 -0400

    using if stmt rather than switch

--- packages/mimir/src/rules/no-useless-spread.ts
+++ packages/mimir/src/rules/no-useless-spread.ts
@@ -5,22 +5,18 @@ const FAILURE_STRING = 'Using the spread operator here is not necessary.';
 
 export class Rule extends AbstractRule {
     public apply() {
-        for (const node of this.context.getFlatAst()) {
-            switch (node.kind) {
-                case ts.SyntaxKind.SpreadElement:
-                    if ((<ts.SpreadElement>node).expression.kind === ts.SyntaxKind.ArrayLiteralExpression)
-                        this.addFailureAtNode(node, FAILURE_STRING, removeUselessSpread(<ts.SpreadElement>node));
-                    continue;
-
-                case ts.SyntaxKind.SpreadAssignment:
-                    if ((<ts.SpreadAssignment>node).expression.kind === ts.SyntaxKind.ObjectLiteralExpression)
-                        this.addFailureAtNode(node, FAILURE_STRING, removeUselessSpread(<ts.SpreadAssignment>node));
-            }
-        }
+        for (const node of this.context.getFlatAst())
+            if (
+                (node.kind === ts.SyntaxKind.SpreadElement &&
+                    (<ts.SpreadElement>node).expression.kind === ts.SyntaxKind.ArrayLiteralExpression) ||
+                (node.kind === ts.SyntaxKind.SpreadAssignment &&
+                    (<ts.SpreadAssignment>node).expression.kind === ts.SyntaxKind.ObjectLiteralExpression)
+            )
+                this.addFailureAtNode(node, FAILURE_STRING, removeUselessSpread(<ts.SpreadElement>node));
     }
 }
 
-function removeUselessSpread(node: ts.SpreadElement | ts.SpreadAssignment): Replacement[] {
+function removeUselessSpread(node: ts.SpreadElement): Replacement[] {
     return [
         Replacement.delete(node.getStart(), node.expression.getStart() + 1),
         Replacement.delete(node.expression.end - 1, node.expression.end),
