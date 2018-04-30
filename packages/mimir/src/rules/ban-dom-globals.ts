57af1db1bed0fad361b8c6de41275c4d562752dc
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-04-30 17:15:37 +0200

    correcly handle ES3 and ES5 without DOM

--- packages/mimir/src/rules/ban-dom-globals.ts
+++ packages/mimir/src/rules/ban-dom-globals.ts
@@ -1,10 +1,12 @@
-import { TypedRule, requireLibraryFile } from '@fimbul/ymir';
+import { TypedRule, excludeDeclarationFiles } from '@fimbul/ymir';
 import * as ts from 'typescript';
-import { getUsageDomain, UsageDomain, isSourceFile } from 'tsutils';
+import { getUsageDomain, UsageDomain, isSourceFile, isInterfaceDeclaration } from 'tsutils';
 import * as path from 'path';
 
-@requireLibraryFile('lib.dom.d.ts')
+@excludeDeclarationFiles
 export class Rule extends TypedRule {
+    private window: ts.Type | undefined = undefined;
+
     public apply() {
         for (const node of this.context.getFlatAst()) {
             if (node.kind !== ts.SyntaxKind.Identifier)
@@ -17,7 +19,7 @@ export class Rule extends TypedRule {
                 symbol !== undefined &&
                 symbol.valueDeclaration !== undefined &&
                 symbol.flags & (ts.SymbolFlags.Variable | ts.SymbolFlags.Function) &&
-                isDeclarationInLibDom(symbol.valueDeclaration)
+                this.isDomGlobal(symbol.valueDeclaration, text)
             )
                 this.addFailureAtNode(
                     node,
@@ -25,20 +27,34 @@ export class Rule extends TypedRule {
                 );
         }
     }
+
+    private isDomGlobal(declaration: ts.Node, name: string): boolean {
+        const sourceFile = getLibFile(declaration);
+        return sourceFile !== undefined && this.isWindowProperty(sourceFile, name);
+    }
+
+    private isWindowProperty(sourceFile: ts.SourceFile, name: string): boolean {
+        if (this.window === undefined) {
+            const declaration =
+                sourceFile.statements.find((d): d is ts.InterfaceDeclaration => isInterfaceDeclaration(d) && d.name.text === 'Window')!;
+            this.window = this.checker.getDeclaredTypeOfSymbol(this.checker.getSymbolAtLocation(declaration.name)!);
+        }
+        return this.window.getProperty(name) !== undefined;
+    }
 }
 
-function isDeclarationInLibDom(node: ts.Node): boolean {
+function getLibFile(node: ts.Node): ts.SourceFile | undefined {
     if (node.kind === ts.SyntaxKind.FunctionDeclaration) {
         node = node.parent!;
     } else if (node.kind === ts.SyntaxKind.VariableDeclaration) {
         node = node.parent!.parent!;
         if (node.kind !== ts.SyntaxKind.VariableStatement)
-            return false;
+            return;
         node = node.parent!;
     } else {
-        return false;
+        return;
     }
-    return isSourceFile(node) && path.basename(node.fileName) === 'lib.dom.d.ts';
+    return isSourceFile(node) && /^lib(?:\.dom)?\.d\.ts$/.test(path.basename(node.fileName)) ? node : undefined;
 }
 
 function isWhitelisted(name: string): boolean {
