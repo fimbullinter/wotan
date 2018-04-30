ab1f122f959993f2119301d8100e12331bc64875
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-04-30 17:03:06 +0200

    new rule: no-useless-declare (#192)

---
+++ packages/mimir/test/no-useless-declare/test.ts
@@ -0,0 +1,47 @@
+declare enum E {}
+export declare const enum CE {}
+declare type T = any;
+export declare interface I {}
+declare class C {}
+
+enum E2 {}
+const enum CE2 {}
+type T2 = any;
+interface I2 {}
+class C2 {}
+
+namespace ns {
+    declare enum E {}
+    declare const enum CE {}
+    declare type T = any;
+    declare interface I {}
+    declare class C {}
+}
+
+namespace ns.sub {
+    declare enum E {}
+    declare const enum CE {}
+    declare type T = any;
+    declare interface I {}
+    declare class C {}
+}
+
+declare namespace ambient {
+    declare type T = any;
+    interface I {}
+}
+
+declare module 'foo' {
+    declare type T = any;
+}
+
+declare module 'bar';
+
+module 'baz';
+
+declare global {
+    declare type T = any;
+}
+
+type declare = any;
+let declare: declare;
