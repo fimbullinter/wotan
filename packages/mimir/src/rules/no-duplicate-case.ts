4ca7bab5277df42e125037fb2cbf21eee412cdd1
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-05-04 19:53:20 +0200

    no-duplicate-case: handle unions of literal types
    Fixes: #225

--- packages/mimir/src/rules/no-duplicate-case.ts
+++ packages/mimir/src/rules/no-duplicate-case.ts
@@ -1,6 +1,6 @@
 import { AbstractRule, excludeDeclarationFiles } from '@fimbul/ymir';
 import * as ts from 'typescript';
-import { isTextualLiteral, isNumericLiteral, isPrefixUnaryExpression, isIdentifier, isLiteralType } from 'tsutils';
+import { isTextualLiteral, isNumericLiteral, isPrefixUnaryExpression, isIdentifier, isLiteralType, unionTypeParts } from 'tsutils';
 import { switchStatements } from '../utils';
 
 @excludeDeclarationFiles
@@ -18,53 +18,69 @@ export class Rule extends AbstractRule {
                     continue;
                 }
                 expressionsSeen.add(text);
-                this.getLiteralValue(clause.expression, (v) => {
-                    if (valuesSeen.has(v))
-                        return this.addFailureAtNode(
-                            clause.expression,
-                            `Duplicate 'case ${typeof v === 'string' ? `"${v}"` : String(v)}'.`,
-                        );
-                    valuesSeen.add(v);
-                });
+                const literals = this.getLiteralValue(clause.expression);
+                switch (literals.length) {
+                    case 0:
+                        break;
+                    case 1:
+                        if (valuesSeen.has(literals[0]))
+                            this.addFailureAtNode(clause.expression, `Duplicate 'case ${formatPrimitive(literals[0])}'.`);
+                        valuesSeen.add(literals[0]);
+                        break;
+                    default:
+                        // union of literal types, do not add these to `valuesSeen`, but display an error if all literals were already seen
+                        if (literals.every((v) => valuesSeen.has(v)))
+                            this.addFailureAtNode(clause.expression, `Duplicate 'case ${literals.map(formatPrimitive).join(' | ')}'.`);
+                }
             }
         }
     }
 
-    private getLiteralValue(node: ts.Expression, cb: (v: Primitive) => void) {
+    private getLiteralValue(node: ts.Expression): Primitive[] {
         let prefixFn: ApplyPrefixFn = identity;
         while (isPrefixUnaryExpression(node)) {
             const next = makePrefixFn(node, prefixFn);
             if (next === undefined)
-                return;
+                return [];
             prefixFn = next;
             node = node.operand;
         }
         if (isTextualLiteral(node))
-            return cb(prefixFn(node.text));
+            return [prefixFn(node.text)];
         if (isNumericLiteral(node))
-            return cb(prefixFn(+node.text));
+            return [prefixFn(+node.text)];
         if (node.kind === ts.SyntaxKind.NullKeyword)
-            return cb(prefixFn(null)); // tslint:disable-line:no-null-keyword
+            return [prefixFn(null)]; // tslint:disable-line:no-null-keyword
         if (isIdentifier(node) && node.originalKeywordKind === ts.SyntaxKind.UndefinedKeyword)
-            return cb(prefixFn(undefined));
+            return [prefixFn(undefined)];
         if (node.kind === ts.SyntaxKind.TrueKeyword)
-            return cb(prefixFn(true));
+            return [prefixFn(true)];
         if (node.kind === ts.SyntaxKind.FalseKeyword)
-            return cb(prefixFn(false));
+            return [prefixFn(false)];
 
         if (this.program === undefined)
-            return;
+            return [];
         const checker = this.program.getTypeChecker();
         let type = checker.getTypeAtLocation(node);
         type = checker.getBaseConstraintOfType(type) || type;
-        if (isLiteralType(type))
-            return cb(prefixFn(type.value));
-        if (type.flags & ts.TypeFlags.BooleanLiteral)
-            return cb(prefixFn((<{intrinsicName: string}><{}>type).intrinsicName === 'true'));
-        if (type.flags & ts.TypeFlags.Undefined)
-            return cb(prefixFn(undefined));
-        if (type.flags & ts.TypeFlags.Null)
-            return cb(prefixFn(null)); // tslint:disable-line:no-null-keyword
+        const result = new Set<Primitive>();
+        for (const t of unionTypeParts(type)) {
+            if (isLiteralType(t)) {
+                result.add(prefixFn(t.value));
+            } else if (t.flags & ts.TypeFlags.BooleanLiteral) {
+                result.add(prefixFn((<{intrinsicName: string}><{}>t).intrinsicName === 'true'));
+            } else if (t.flags & ts.TypeFlags.Undefined) {
+                result.add(prefixFn(undefined));
+            } else if (t.flags & ts.TypeFlags.Null) {
+                result.add(prefixFn(null)); // tslint:disable-line:no-null-keyword
+            } else if (t.flags & ts.TypeFlags.Boolean) {
+                result.add(prefixFn(true));
+                result.add(prefixFn(false));
+            } else {
+                return [];
+            }
+        }
+        return Array.from(result);
     }
 }
 
@@ -89,4 +105,8 @@ function makePrefixFn(node: ts.PrefixUnaryExpression, next: ApplyPrefixFn): Appl
         default:
             return;
     }
+}
+
+function formatPrimitive(v: Primitive) {
+    return typeof v === 'string' ? `"${v}"` : String(v);
 }
