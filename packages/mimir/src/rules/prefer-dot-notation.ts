aeb9fb42deb4db598aeb67e6d02c8f5c5d94d9b0
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-05-17 19:14:20 +0200

    Update to ts@2.9.0-rc

--- packages/mimir/src/rules/prefer-dot-notation.ts
+++ packages/mimir/src/rules/prefer-dot-notation.ts
@@ -6,7 +6,9 @@ import { isElementAccessExpression, isTextualLiteral, isValidPropertyAccess } fr
 export class Rule extends AbstractRule {
     public apply() {
         for (const node of this.context.getFlatAst()) {
-            if (isElementAccessExpression(node) && node.argumentExpression !== undefined &&
+            if (isElementAccessExpression(node) &&
+                // for compatiblity with typescript@<2.9.0
+                node.argumentExpression !== undefined && // wotan-disable-line no-useless-predicate
                 isTextualLiteral(node.argumentExpression) && isValidPropertyAccess(node.argumentExpression.text)) {
                 // for compatibility with typescript@2.4
                 const property = ts.unescapeIdentifier(node.argumentExpression.text); // wotan-disable-line no-unstable-api-use
