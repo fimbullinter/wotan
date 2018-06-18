29b7c0678a7f05a1286ad96b6d9f66b5d115f713
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-06-18 23:07:47 +0200

    wip: prefer-exponentiation-operator

---
+++ packages/mimir/src/rules/prefer-exponentiation-operator.ts
@@ -0,0 +1,45 @@
+import { excludeDeclarationFiles, AbstractRule, Replacement } from '@fimbul/ymir';
+import { WrappedAst, getWrappedNodeAtPosition, isIdentifier, isPropertyAccessExpression, isCallExpression, isSpreadElement } from 'tsutils';
+import { expressionNeedsParensWhenReplacingNode } from '../utils';
+import * as ts from 'typescript';
+
+@excludeDeclarationFiles
+export class Rule extends AbstractRule {
+    public apply() {
+        const re = /\bpow\s[/(]/g;
+        let wrappedAst: WrappedAst | undefined;
+        for (let match = re.exec(this.sourceFile.text); match !== null; match = re.exec(this.sourceFile.text)) {
+            const {node} = getWrappedNodeAtPosition(wrappedAst || (wrappedAst = this.context.getWrappedAst()), match.index)!;
+            if (!isIdentifier(node) || node.end !== match.index + 3 || node.text !== 'pow')
+                continue;
+            const parent = node.parent!;
+            if (!isPropertyAccessExpression(parent) || !isIdentifier(parent.expression) || parent.expression.text !== 'Math')
+                continue;
+            const grandparent = parent.parent!;
+            if (
+                !isCallExpression(grandparent) ||
+                grandparent.expression !== parent ||
+                grandparent.arguments.length !== 2 ||
+                grandparent.arguments.some(isSpreadElement)
+            )
+                continue;
+            const fix = [Replacement.replace(grandparent.arguments[1].pos - 1, grandparent.arguments[1].pos, '**')];
+            if (expressionNeedsParensWhenReplacingNode(
+                    ts.createBinary(grandparent.arguments[0], ts.SyntaxKind.AsteriskAsteriskToken, grandparent.arguments[1]),
+                    grandparent,
+                )) {
+                fix.push(Replacement.delete(grandparent.getStart(this.sourceFile), grandparent.arguments[0].pos - 1));
+            } else {
+                fix.push(
+                    Replacement.delete(grandparent.getStart(this.sourceFile), grandparent.arguments[0].getStart(this.sourceFile)),
+                    Replacement.delete(grandparent.end - 1, grandparent.end),
+                );
+            }
+            this.addFailureAtNode(
+                grandparent,
+                "Prefer the exponentiation operator '**' over 'Math.pow'.",
+                fix,
+            );
+        }
+    }
+}
