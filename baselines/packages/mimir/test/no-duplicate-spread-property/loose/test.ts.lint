19f1e9b1e250a2d9b4137e9ad2a81982088fdcf4
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-08-09 13:59:22 +0200

    no-duplicate-spread-property: exclude class Getters and Setter-only properties
    Fixes: #371

--- baselines/packages/mimir/test/no-duplicate-spread-property/loose/test.ts.lint
+++ baselines/packages/mimir/test/no-duplicate-spread-property/loose/test.ts.lint
@@ -174,3 +174,36 @@ var v: any;
         bar: 2,
     },
 });
+
+({
+    foo: 1,
+    ...new class {
+        get foo() { return 1; }
+        bar = 1;
+    },
+});
+
+({
+    foo: 1,
+    ...{
+        get foo() { return 1; },
+        bar: 2,
+    },
+});
+
+({
+    foo: 1,
+    ...{
+        get foo() { return 1; },
+        set foo(v: number) {},
+        bar: 2,
+    },
+});
+
+({
+    foo: 1,
+    ...{
+        set foo(v: number) {},
+        bar: 2,
+    },
+});
--- packages/mimir/src/rules/no-duplicate-spread-property.ts
+++ packages/mimir/src/rules/no-duplicate-spread-property.ts
@@ -98,7 +98,7 @@ function getPropertyInfoFromType(type: ts.Type): PropertyInfo {
         assignedNames: [],
     };
     for (const prop of type.getProperties()) {
-        if (isClassMethod(prop))
+        if (!isSpreadableProperty(prop))
             continue;
         if ((prop.flags & ts.SymbolFlags.Optional) === 0)
             result.assignedNames.push(prop.escapedName);
@@ -106,12 +106,15 @@ function getPropertyInfoFromType(type: ts.Type): PropertyInfo {
     }
     return result;
 }
-function isClassMethod(prop: ts.Symbol): boolean | undefined {
-    if (prop.flags & ts.SymbolFlags.Method && prop.declarations !== undefined)
-        for (const declaration of prop.declarations)
+function isSpreadableProperty(prop: ts.Symbol): boolean | undefined {
+    if (prop.flags & (ts.SymbolFlags.Method | ts.SymbolFlags.GetAccessor)) {
+        for (const declaration of prop.declarations!)
             if (isClassLikeDeclaration(declaration.parent!))
-                return true;
-    return false;
+                return false; // class Methods and GetAccessors are not spreadable
+    } else if (prop.flags & ts.SymbolFlags.SetAccessor) {
+        return false; // SetAccessor-only properties are never spreadable
+    }
+    return true;
 }
 
 function combinePropertyInfo(a: PropertyInfo, b: PropertyInfo): PropertyInfo {
