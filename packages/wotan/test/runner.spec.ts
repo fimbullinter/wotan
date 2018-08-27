6af609d2173126a623b4acb7f7e2fd64c3f6a701
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-08-27 14:08:11 +0200

    Add test for symlinks in typeRoots
    Ref: #387

--- packages/wotan/test/runner.spec.ts
+++ packages/wotan/test/runner.spec.ts
@@ -1,18 +1,19 @@
 import 'reflect-metadata';
 import test from 'ava';
-import { Container, injectable } from 'inversify';
+import { Container, injectable, BindingScopeEnum } from 'inversify';
 import { createCoreModule } from '../src/di/core.module';
 import { createDefaultModule } from '../src/di/default.module';
 import { Runner } from '../src/runner';
 import * as path from 'path';
 import { NodeFileSystem } from '../src/services/default/file-system';
 import { FileSystem, MessageHandler, DirectoryService } from '@fimbul/ymir';
+import { unixifyPath } from '../src/utils';
 
 const directories: DirectoryService = {
     getCurrentDirectory() { return path.resolve('packages/wotan'); },
 };
 test('throws error on non-existing file', (t) => {
-    const container = new Container();
+    const container = new Container({defaultScope: BindingScopeEnum.Singleton});
     container.bind(DirectoryService).toConstantValue(directories);
     container.load(createCoreModule({}), createDefaultModule());
     const runner = container.get(Runner);
@@ -35,7 +36,7 @@ test('throws error on non-existing file', (t) => {
 });
 
 test('throws error on file not included in project', (t) => {
-    const container = new Container();
+    const container = new Container({defaultScope: BindingScopeEnum.Singleton});
     container.bind(DirectoryService).toConstantValue(directories);
     container.load(createCoreModule({}), createDefaultModule());
     const runner = container.get(Runner);
@@ -57,7 +58,7 @@ test('throws error on file not included in project', (t) => {
 });
 
 test('throws if no tsconfig.json can be found', (t) => {
-    const container = new Container();
+    const container = new Container({defaultScope: BindingScopeEnum.Singleton});
     @injectable()
     class MockFileSystem extends NodeFileSystem {
         constructor(logger: MessageHandler) {
@@ -114,16 +115,13 @@ test('throws if no tsconfig.json can be found', (t) => {
 });
 
 test('reports warnings while parsing tsconfig.json', (t) => {
-    const container = new Container();
+    const container = new Container({defaultScope: BindingScopeEnum.Singleton});
     const files: {[name: string]: string | undefined} = {
         'invalid-config.json': '{',
         'invalid-base.json': '{"extends": "./invalid-config.json"}',
         'invalid-files.json': '{"files": []}',
         'no-match.json': '{"include": ["non-existent"], "compilerOptions": {"noLib": true}}',
     };
-    function isLibraryFile(name: string) {
-        return /[\\/]typescript[\\/]lib[\\/]lib(\.es\d+(\.\w+)*)?\.d\.ts$/.test(name);
-    }
     @injectable()
     class MockFileSystem extends NodeFileSystem {
         constructor(logger: MessageHandler) {
@@ -203,3 +201,114 @@ test('reports warnings while parsing tsconfig.json', (t) => {
     }));
     t.regex(warning, /^error TS18003:/);
 });
+
+test('excludes symlinked typeRoots', (t) => {
+    const container = new Container({defaultScope: BindingScopeEnum.Singleton});
+    container.bind(DirectoryService).toConstantValue(directories);
+    interface FileMeta {
+        content?: string;
+        symlink?: string;
+        entries?: Record<string, FileMeta | undefined>;
+    }
+    const files: FileMeta = {
+        entries: {
+            'tsconfig.json': {content: '{"files": ["a.ts"]}'},
+            'a.ts': {content: 'foo;'},
+            node_modules: {
+                entries: {
+                    '@types': {
+                        entries: {
+                            foo: {symlink: 'foo'},
+                        },
+                    },
+                },
+            },
+            foo: {
+                entries: {
+                    'index.d.ts': {content: 'export {};'},
+                },
+            },
+            '.wotanrc.yaml': {content: 'rules: {trailing-newline: error}'},
+        },
+    };
+    @injectable()
+    class MockFileSystem extends NodeFileSystem {
+        constructor(private dirs: DirectoryService, logger: MessageHandler) {
+            super(logger);
+        }
+        public stat(file: string) {
+            if (isLibraryFile(file))
+                return super.stat(file);
+            const f = this.resolvePath(file);
+            if (f === undefined)
+                throw new Error('ENOENT');
+            return {
+                isFile() { return f.resolved.content !== undefined; },
+                isDirectory() { return f.resolved.content === undefined; },
+            };
+        }
+        public readFile(file: string) {
+            if (isLibraryFile(file))
+                return super.readFile(file);
+            const f = this.resolvePath(file);
+            if (f === undefined)
+                throw new Error('ENOENT');
+            if (f.resolved.content === undefined)
+                throw new Error('EISDIR');
+            return f.resolved.content;
+        }
+        public readDirectory(dir: string): string[] {
+            const f = this.resolvePath(dir);
+            if (f === undefined)
+                throw new Error('ENOENT');
+            if (f.resolved.content !== undefined)
+                throw new Error('ENOTDIR');
+            return Object.keys(f.resolved.entries || {});
+        }
+        public realpath(file: string): string {
+            if (isLibraryFile(file))
+                return super.realpath(file);
+            const f = this.resolvePath(file);
+            if (f === undefined)
+                throw new Error('ENOENT');
+            return path.resolve(this.dirs.getCurrentDirectory(), f.realpath);
+        }
+
+        private resolvePath(p: string) {
+            const parts = path.relative(this.normalizePath(this.dirs.getCurrentDirectory()), p).split(/\//g);
+            let current: FileMeta | undefined = files;
+            let part = parts.shift();
+            let realPath = [];
+            while (current !== undefined && part !== undefined) {
+                if (part) {
+                    realPath.push(part);
+                    current = current.entries && current.entries[part];
+                    if (current !== undefined && current.symlink !== undefined) {
+                        parts.unshift(...current.symlink.split(/\//g));
+                        realPath = [];
+                        current = files;
+                    }
+                }
+                part = parts.shift();
+            }
+            return current && {resolved: current, realpath: realPath.join('/')};
+        }
+    }
+    container.bind(FileSystem).to(MockFileSystem);
+    container.load(createCoreModule({}), createDefaultModule());
+    const runner = container.get(Runner);
+    const result = Array.from(runner.lintCollection({
+        config: undefined,
+        files: [],
+        exclude: [],
+        project: 'tsconfig.json',
+        fix: false,
+        extensions: undefined,
+    }));
+    t.is(result.length, 1);
+    t.is(result[0][0], unixifyPath(path.resolve('packages/wotan/a.ts')));
+});
+
+function isLibraryFile(name: string) {
+    return /[\\/]typescript[\\/]lib[\\/]lib(\.es\d+(\.\w+)*)?\.d\.ts$/.test(name);
+}
