162354add6938be0aaa65ae9b649d0160bba5c04
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-11-30 19:38:25 +0100

    drop support for typescript 2.8 and 2.9

--- packages/mimir/src/rules/no-inferred-empty-object.ts
+++ packages/mimir/src/rules/no-inferred-empty-object.ts
@@ -2,8 +2,6 @@ import { TypedRule, excludeDeclarationFiles, typescriptOnly } from '@fimbul/ymir
 import * as ts from 'typescript';
 import { isTypeLiteralNode } from 'tsutils';
 
-const typescriptPre290 = /^2\.[78]\./.test(ts.version);
-
 @excludeDeclarationFiles
 @typescriptOnly // in .js files TypeParameters default to `any`
 export class Rule extends TypedRule {
@@ -15,13 +13,14 @@ export class Rule extends TypedRule {
             } else if (node.kind === ts.SyntaxKind.NewExpression) {
                 if ((<ts.NewExpression>node).typeArguments === undefined)
                     this.checkNewExpression(<ts.NewExpression>node);
-            } else if (!typescriptPre290 && // type arguments on JSX elements and tagged templates was introduced in typescript@2.9.0
-                       (
-                           node.kind === ts.SyntaxKind.JsxOpeningElement ||
-                           node.kind === ts.SyntaxKind.JsxSelfClosingElement ||
-                           node.kind === ts.SyntaxKind.TaggedTemplateExpression
-                        ) &&
-                       (<ts.JsxOpeningLikeElement | ts.TaggedTemplateExpression>node).typeArguments === undefined) {
+            } else if (
+                (
+                    node.kind === ts.SyntaxKind.JsxOpeningElement ||
+                    node.kind === ts.SyntaxKind.JsxSelfClosingElement ||
+                    node.kind === ts.SyntaxKind.TaggedTemplateExpression
+                ) &&
+                (<ts.JsxOpeningLikeElement | ts.TaggedTemplateExpression>node).typeArguments === undefined
+            ) {
                 this.checkCallExpression(<ts.JsxOpeningLikeElement>node);
             }
         }
@@ -29,22 +28,19 @@ export class Rule extends TypedRule {
 
     private checkCallExpression(node: ts.CallExpression | ts.JsxOpeningLikeElement | ts.TaggedTemplateExpression) {
         const signature = this.checker.getResolvedSignature(node)!;
-        // wotan-disable-next-line no-useless-predicate
         if (signature.declaration !== undefined) {
             const typeParameters = ts.getEffectiveTypeParameterDeclarations(signature.declaration);
-            if (typeParameters !== undefined && typeParameters.length !== 0) // wotan-disable-line no-useless-predicate
+            if (typeParameters.length !== 0)
                 return this.checkInferredTypeParameters(signature, typeParameters, node);
         }
     }
 
     private checkNewExpression(node: ts.NewExpression) {
         const signature = this.checker.getResolvedSignature(node)!;
-        // wotan-disable-next-line no-useless-predicate
         if (signature.declaration !== undefined) {
             // There is an explicitly declared construct signature
             const typeParameters = ts.getEffectiveTypeParameterDeclarations(signature.declaration);
-            // wotan-disable-next-line no-useless-predicate
-            if (typeParameters !== undefined && typeParameters.length !== 0) // only check the signature if it declares type parameters
+            if (typeParameters.length !== 0) // only check the signature if it declares type parameters
                 return this.checkInferredTypeParameters(signature, typeParameters, node);
         }
 
@@ -56,8 +52,6 @@ export class Rule extends TypedRule {
         const typeParameterResult = [];
         for (const declaration of <ts.DeclarationWithTypeParameters[]>symbol.declarations) {
             const typeParameters = ts.getEffectiveTypeParameterDeclarations(declaration);
-            if (typeParameters === undefined) // wotan-disable-line no-useless-predicate
-                continue; // compatibility with typescript@<2.9.0
             for (let i = 0; i < typeParameters.length; ++i)
                 // wotan-disable-next-line no-useless-predicate
                 if (typeParameterResult[i] === undefined || typeParameters[i].default !== undefined)
