ed880f2496d990163e5f157ccb9483a0a17a6d8c
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-01-05 15:39:38 +0100

    even more test coverage

--- packages/wotan/test/line-switches/useless.ts
+++ packages/wotan/test/line-switches/useless.ts
@@ -2,7 +2,7 @@
 // wotan-disable
 // wotan-disable
 // wotan-enable no-unused-label
-// wotan-disable baz, my/cool/alias, no-unused-label
+// wotan-disable baz, no-unused-label, my/cool/alias, no-unused-label
 // wotan-enable-line
 // wotan-enable-line baz, bar
 // wotan-enable baz, my/cool/alias, my/cool/alias
