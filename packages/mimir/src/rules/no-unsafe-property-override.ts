bcec74264d51dfad59ce9bef3ac5cce2a7472365
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-05-07 22:45:28 +0200

    all the things readonly

--- packages/mimir/src/rules/no-unsafe-property-override.ts
+++ packages/mimir/src/rules/no-unsafe-property-override.ts
@@ -1,5 +1,19 @@
 import { TypedRule } from '@fimbul/ymir';
-import { WrappedAst, getWrappedNodeAtPosition, isPropertyDeclaration } from 'tsutils';
+import {
+    WrappedAst,
+    getWrappedNodeAtPosition,
+    isPropertyDeclaration,
+    isUnionOrIntersectionType,
+    isInConstContext,
+    isObjectFlagSet,
+    isObjectType,
+    isTypeReference,
+    isTupleType,
+    isPropertyAccessExpression,
+    isIdentifier,
+    isEntityNameExpression,
+    isPropertyAssignment,
+} from 'tsutils';
 import * as ts from 'typescript';
 import { getPropertyOfType } from '../utils';
 
@@ -28,7 +42,7 @@ export class Rule extends TypedRule {
                 if (baseType === undefined)
                     baseType = this.checker.getTypeAtLocation(node.types[0]);
                 const parentProperty = getPropertyOfType(baseType, symbol.escapedName);
-                if (parentProperty === undefined || !isReadonlySymbol(parentProperty))
+                if (parentProperty === undefined || !isReadonlySymbol(parentProperty, baseType, this.checker))
                     continue;
                 this.addFindingAtNode(
                     member.name,
@@ -39,12 +53,93 @@ export class Rule extends TypedRule {
     }
 }
 
-function isReadonlySymbol(symbol: ts.Symbol) {
-    return (symbol.flags & ts.SymbolFlags.Accessor) === ts.SymbolFlags.GetAccessor ||
-        symbol.declarations !== undefined && symbol.declarations.some((d) => ts.getCombinedModifierFlags(d) & ts.ModifierFlags.Readonly);
-    // TODO handle Object.defineProperty in JS
-    // TODO readonly mapped types
-    // TODO readonly index signature
-    // TODO readonly tuples?
-    // TODO object literal in const context
+function isReadonlySymbol(symbol: ts.Symbol, parentType: ts.Type, checker: ts.TypeChecker) {
+    if (symbol.flags & ts.SymbolFlags.GetAccessor)
+        return (symbol.flags & ts.SymbolFlags.SetAccessor) === 0;
+    return symbol.declarations !== undefined && containsReadonlyPropertyDeclaration(symbol.declarations, checker) ||
+        (symbol.flags & ts.SymbolFlags.Transient) !== 0 && (
+            /^(?:[1-9]\d*|0)$/.test(symbol.name) && distributeCheck(parentType, symbol.escapedName, isElementOfReadonlyTuple) ||
+            distributeCheck(parentType, symbol.escapedName, isReadonlyPropertyFromMappedType)
+        );
+        // TODO readonly index signature
+}
+
+function containsReadonlyPropertyDeclaration(declarations: ReadonlyArray<ts.Declaration>, checker: ts.TypeChecker) {
+    for (const node of declarations) {
+        if (ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Readonly)
+            return true;
+        switch (node.kind) {
+            case ts.SyntaxKind.PropertyAssignment:
+            case ts.SyntaxKind.ShorthandPropertyAssignment:
+                if (isInConstContext(<ts.ObjectLiteralExpression>node.parent))
+                    return true;
+                break;
+            case ts.SyntaxKind.CallExpression:
+                if (isReadonlyAssignmentDeclaration(<ts.CallExpression>node, checker))
+                    return true;
+        }
+    }
+    return false;
+}
+
+function isReadonlyAssignmentDeclaration(node: ts.CallExpression, checker: ts.TypeChecker) {
+    if (!isBindableObjectDefinePropertyCall(node))
+        return false;
+    const descriptorType = checker.getTypeAtLocation(node.arguments[2]);
+    if (descriptorType.getProperty('value') === undefined)
+        return descriptorType.getProperty('set') === undefined;
+    const writableProp = descriptorType.getProperty('writable');
+    if (writableProp === undefined)
+        return false;
+    const writableType = writableProp.valueDeclaration !== undefined && isPropertyAssignment(writableProp.valueDeclaration)
+        ? checker.getTypeAtLocation(writableProp.valueDeclaration.initializer)
+        : checker.getTypeOfSymbolAtLocation(writableProp, node.arguments[2]);
+    return (writableType.flags & ts.TypeFlags.BooleanLiteral) !== 0 &&
+        (<{intrinsicName: string}><{}>writableType).intrinsicName === 'false';
+
+}
+
+function isBindableObjectDefinePropertyCall(node: ts.CallExpression) {
+    return node.arguments.length === 3 &&
+        isEntityNameExpression(node.arguments[0]) &&
+        isNumericOrStringLikeLiteral(node.arguments[1]) &&
+        isPropertyAccessExpression(node.expression) &&
+        node.expression.name.escapedText === 'defineProperty' &&
+        isIdentifier(node.expression.expression) &&
+        node.expression.expression.escapedText === 'Object';
+}
+
+function isNumericOrStringLikeLiteral(node: ts.Expression) {
+    switch (node.kind) {
+        case ts.SyntaxKind.StringLiteral:
+        case ts.SyntaxKind.NumericLiteral:
+        case ts.SyntaxKind.NoSubstitutionTemplateLiteral:
+            return true;
+        default:
+            return false;
+    }
+}
+
+function distributeCheck(type: ts.Type, name: ts.__String, cb: (type: ts.Type, name: ts.__String) => boolean) {
+    if (isUnionOrIntersectionType(type)) {
+        for (const t of type.types)
+            if (distributeCheck(t, name, cb))
+                return true;
+        return false;
+    }
+
+    return cb(type, name);
+}
+
+function isElementOfReadonlyTuple(type: ts.Type, name: ts.__String): boolean {
+    return isTypeReference(type) && isTupleType(type.target) && type.target.readonly && getPropertyOfType(type.target, name) !== undefined;
+}
+
+function isReadonlyPropertyFromMappedType(type: ts.Type, name: ts.__String): boolean {
+    if (!isObjectType(type) || !isObjectFlagSet(type, ts.ObjectFlags.Mapped))
+        return false;
+    const declaration = <ts.MappedTypeNode>type.symbol!.declarations![0];
+    if (declaration.readonlyToken !== undefined)
+        return declaration.readonlyToken.kind !== ts.SyntaxKind.MinusToken;
+    return distributeCheck((<{modifiersType: ts.Type}><unknown>type).modifiersType, name, isReadonlyPropertyFromMappedType);
 }
--- packages/mimir/test/no-unsafe-property-override/test.ts
+++ packages/mimir/test/no-unsafe-property-override/test.ts
@@ -1,8 +1,14 @@
+import {jsObj} from './prop';
+
 class Base {
     get prop() { return 1; }
     get prop2() { return 1; }
     get prop3() { return 1; }
     get prop4() { return 1; }
+    get prop5() { return 1; }
+    set prop5(_) {}
+    get [name]() { return 1; }
+    get [Symbol.toStringTag]() {return 'A'};
 }
 const prop4 = 'prop4';
 class Derived extends Base {
@@ -10,4 +16,50 @@ class Derived extends Base {
     'prop2' = 1;
     'prop3' = 1;
     [prop4] = 1;
+    prop5 = 1;
+    [name] = 1;
+    [Symbol.toStringTag] = 'B';
+}
+
+class Derived2 extends ((): new () => Readonly<Record<'foo', number>> => undefined!)() {
+    foo = 1;
+}
+
+class Derived3 extends ((): new () => {readonly [K in 'bar']: number} => undefined!)() {
+    bar = 1;
+}
+
+type Mutable<T> = {-readonly [K in keyof T]: T[K]};
+
+class Derived4 extends ((): new () => Mutable<Readonly<Record<'baz', number>>> => undefined!)() {
+    baz = 1;
+}
+
+class Derived5 extends ((): new () => Pick<Readonly<Record<'bas', number>>, 'bas'> => undefined!)() {
+    bas = 1;
+}
+
+class Derived6 extends ((): new () => Partial<Readonly<Record<'foo', number>>> & Pick<Readonly<{bar: number} & {baz: number}>, 'baz'> => undefined!)() {
+    foo = 1;
+    bar = 1;
+    baz = 1;
+}
+
+const obj = {const: 1 + 1} as const;
+
+class Derived7 extends ((): new() => typeof obj => undefined!)() {
+    const = 1;
+}
+
+class Derived8 extends ((): new() => readonly [number, string] => undefined!)() {
+    0 = 1;
+}
+
+class TsClass extends ((): new() => typeof jsObj => undefined!)() {
+    a = 1;
+    b = 1;
+    c = 1;
+    d = 1;
+    e = 1;
+    f = 1;
 }
