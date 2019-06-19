926f48ed33596bb11b529a10d137da686ee87217
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-06-19 13:08:41 +0200

    use glob matching to determine unmatched patterns

--- packages/wotan/src/runner.ts
+++ packages/wotan/src/runner.ts
@@ -256,11 +256,15 @@ export class Runner {
 
         const allMatchedFiles: string [] = [];
         const include: IMinimatch[] = [];
-        const nonMagicGlobs = [];
+        const nonMagicGlobs: NonMagicGlob[] = [];
         for (const pattern of patterns) {
-            if (!pattern.hasMagic)
-                nonMagicGlobs.push(pattern.normalized[0]);
-            include.push(...pattern.normalized.map((p) => new Minimatch(p)));
+            if (!pattern.hasMagic) {
+                const mm = new Minimatch(pattern.normalized[0]);
+                nonMagicGlobs.push({raw: pattern.normalized[0], match: mm});
+                include.push(mm);
+            } else {
+                include.push(...pattern.normalized.map((p) => new Minimatch(p)));
+            }
         }
         const ex = exclude.map((p) => new Minimatch(p, {dot: true}));
         const projectsSeen: string[] = [];
@@ -400,10 +404,15 @@ function getFiles(patterns: ReadonlyArray<NormalizedGlob>, exclude: ReadonlyArra
     return new Set(result.map(unixifyPath)); // deduplicate files
 }
 
-function ensurePatternsMatch(include: string[], exclude: IMinimatch[], files: string[], projects: ReadonlyArray<string>) {
+interface NonMagicGlob {
+    raw: string;
+    match: IMinimatch;
+}
+
+function ensurePatternsMatch(include: NonMagicGlob[], exclude: IMinimatch[], files: string[], projects: ReadonlyArray<string>) {
     for (const pattern of include)
-        if (!files.includes(pattern) && !isExcluded(pattern, exclude))
-            throw new ConfigurationError(`'${pattern}' is not included in any of the projects: '${projects.join("', '")}'.`);
+        if (!isExcluded(pattern.raw, exclude) && !files.some((f) => pattern.match.match(f)))
+            throw new ConfigurationError(`'${pattern.raw}' is not included in any of the projects: '${projects.join("', '")}'.`);
 }
 
 function isExcluded(file: string, exclude: IMinimatch[]): boolean {
