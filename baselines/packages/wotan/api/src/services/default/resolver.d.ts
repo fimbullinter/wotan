d6d497e07fa9584663ee11eb5e8c4f142c29c984
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-12-31 19:50:52 +0100

    update TypeScript, drop support for older versions (#673)

--- baselines/packages/wotan/api/src/services/default/resolver.d.ts
+++ baselines/packages/wotan/api/src/services/default/resolver.d.ts
@@ -2,8 +2,8 @@ import { Resolver, DirectoryService } from '@fimbul/ymir';
 import { CachedFileSystem } from '../cached-file-system';
 export declare class NodeResolver implements Resolver {
     constructor(fs: CachedFileSystem, directories: DirectoryService);
-    getDefaultExtensions(): ReadonlyArray<string>;
-    resolve(id: string, basedir?: string, extensions?: ReadonlyArray<string>, paths?: ReadonlyArray<string>): string;
+    getDefaultExtensions(): readonly string[];
+    resolve(id: string, basedir?: string, extensions?: readonly string[], paths?: ReadonlyArray<string>): string;
     require(id: string, options?: {
         cache?: boolean;
     }): any;
